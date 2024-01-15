26fc708ce000d8b90e1d4f36eacb3d94c4dec6d6
navi04399 <30684819-navi04399@users.noreply.replit.com>
2024-01-15 16:10:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let app = express();
+let https = require("https");
+let cors = require("cors");
+
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/weatherData", function (request, response) {
+  console.log("Server called");
+  const apiUrl =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-12-15T13:00:00?key=8QKZBL4WMDPPKEY3BSBNPPFUV&include=current&contentType=json";
+
+  https.get(apiUrl, (res) => {
+    let data = "";
+    let JSONData;
+
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      JSONData = JSON.parse(data);
+
+      response.json({
+        temperature: JSONData.currentConditions.temp,
+        humidity: JSONData.currentConditions.humidity,
+        pressure: JSONData.currentConditions.pressure,
+      });
+    });
+  });
+});
+
+let listener = app.listen(process.env.PORT || 3000, () => {
+  console.log("App is Listening" + listener.address().port);
+});
